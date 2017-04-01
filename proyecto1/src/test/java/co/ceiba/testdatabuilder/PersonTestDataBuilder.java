a02eba73e730d5ef4ad77042df5e012016da3eb8
Sebastián Cardona <pana1994@hotmail.com>
2017-04-01 11:56:58 -0500

    GRAL:FFF

---
+++ proyecto1/src/test/java/co/ceiba/testdatabuilder/PersonTestDataBuilder.java
@@ -0,0 +1,23 @@
+package co.ceiba.testdatabuilder;
+
+import co.ceiba.domain.Person;
+
+public class PersonTestDataBuilder {
+	private String name;
+	private String lastName;
+	
+	public PersonTestDataBuilder() {
+		this.name = "Ceiba";
+		this.lastName = "Software House";
+	}
+	
+	public PersonTestDataBuilder withName(String name){
+		this.name = name;
+		return this;
+	}
+	
+	public Person build(){
+		return new Person(this.name,this.lastName);
+	}
+	
+}
